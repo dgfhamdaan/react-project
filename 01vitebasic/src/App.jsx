7711588de5f6ae9f817272d515291e334c2d1096
hamdaan shaikh <hamdaanshaikh@email.com>
2024-02-19 23:49:45 +0530

    create own react library and jsx

--- 01vitebasic/src/App.jsx
+++ 01vitebasic/src/App.jsx
@@ -1,10 +1,12 @@
 import Chai from "./Chai"
 
 function App() {
+  const username = "hamdaan";
+
   return (
     <>
         <Chai/>
-        <h1>dev aur code</h1>
+        <h1>dev aur code {username}</h1>
         <p>welcome developer</p>
     </>
   )
--- 01vitebasic/src/main.jsx
+++ 01vitebasic/src/main.jsx
@@ -2,6 +2,35 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function Myapp (){
+    return(
+        <h1>custom react!</h1>
+    )
+}
+
+// const reactElement = {
+//     type: 'a',
+//     props: {
+//       href: "http://google.com",
+//       target: "_blank"
+//     },
+//     children: "click me to visit google"
+//   }
+
+  const anotherElement = (
+    <a href="http://google.com" target="_blank">google visit</a>
+  )
+
+  const anotherUser = "dgf";
+
+  const reactElement = React.createElement(
+    'a',
+    {href: "http://google.com",target:"_blank"},
+    "click me to visit google",
+    anotherUser
+  )
+  
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    reactElement
 )
