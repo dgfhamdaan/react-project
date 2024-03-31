840ce79d3f3a6459b4fc19f9ceabc7718dff959d
hamdaan shaikh <hamdaanshaikh@email.com>
2024-04-01 02:13:12 +0530

    change

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -35,7 +35,7 @@ function App() {
 
 
   const copyPasswordToClipboard = useCallback(()=> {
-    passwordRef.current?.select();
+    // passwordRef.current?.select();
     passwordRef.current?.setSelectionRange(0,100);
     window.navigator.clipboard.writeText(password)
   },[password])
